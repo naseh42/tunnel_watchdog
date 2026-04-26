d7f3cb7e6ef002b2e1a2034330aa265ddbd5d43a
naseh42 <naseh42@gmail.com>
2026-04-26 18:47:58 +0330

    Update relay.js

--- api/relay.js
+++ api/relay.js
@@ -1,16 +1,3 @@
 export default async function handler(req, res) {
-  const target = process.env.TARGET_DOMAIN || 'https://google.com';
-  
-  try {
-    const response = await fetch(target + req.url, {
-      headers: req.headers,
-      method: req.method,
-      body: req.method !== 'GET' ? JSON.stringify(req.body) : undefined
-    });
-    
-    const data = await response.text();
-    res.status(response.status).send(data);
-  } catch (error) {
-    res.status(500).send('Error: ' + error.message);
-  }
+  res.status(200).send("Vercel relay is alive!");
 }
